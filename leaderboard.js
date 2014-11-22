6d269b8376614271ab32c8adb01f788ed5e501dc
Sean Morrison <seanmorrison112358@gmail.com>
2014-11-21 16:11:00 -0800

    databases part 2: document sorting.

--- leaderboard.js
+++ leaderboard.js
@@ -5,11 +5,19 @@ if (Meteor.isClient) {
     'click .player': function() {
       var playerId = this._id;
       Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .decrement': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
     }
   });
   Template.leaderboard.helpers({
     'player' : function(){
-      return PlayersList.find()
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
     },
     'selectedClass' : function() {
       var playerId = this._id;
